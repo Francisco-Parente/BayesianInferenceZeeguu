c80b69e4580e826e6bbeceaf691dd197d8dab5a8
Francisco-Parente <franciscoparente96@gmail.com>
2025-03-05 00:10:45 +0100

    feat : loading the data intot he py file

--- src/LinearRegressionModel.py
+++ src/LinearRegressionModel.py
@@ -1,3 +1,17 @@
-import pymc
+import arviz as az
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+import pymc as pm
+
+
+bookmark = np.loadtxt("./data/bookmark.csv", dtype = int, delimiter = ",")
+
+bookmark_collumns = [
+        "id", "user_id", "origin_id", "translation_id", "text_id", 
+        "fit_for_study", "learning_cycle", "level","user_preference"
+            ]
 
 
